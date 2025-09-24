d4abd4a26164b2867c936a6fee510556e739ac00
David H <151378521+Kezy-d4@users.noreply.github.com>
2025-09-24 15:14:23 -0400

    feat: extend constants module

--- lib/constants.rb
+++ lib/constants.rb
@@ -4,4 +4,17 @@
 module Constants
   NUMBER_OF_RANKS = 8
   NUMBER_OF_FILES = 8
+
+  SQUARE_COLOR_RGB_MAP = { green: '119;162;109',
+                           yellow: '200;194;100' }.freeze
+
+  PLAYER_COLOR_RGB_MAP = { white: '255;255;255',
+                           black: '0;0;0' }.freeze
+
+  PIECE_UNICODE_ICON_MAP = { king: "\u{265A}",
+                             queen: "\u{265B}",
+                             rook: "\u{265C}",
+                             bishop: "\u{265D}",
+                             knight: "\u{265E}",
+                             pawn: "\u{265F}" }.freeze
 end
