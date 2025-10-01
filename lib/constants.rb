6cdd463ebd04cfecc992d09f74315ee4c1bee9a3
David H <151378521+Kezy-d4@users.noreply.github.com>
2025-10-01 17:13:23 -0400

    reformat: Constants

--- lib/constants.rb
+++ lib/constants.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative 'pieces'
+
 # Namespace to store constants for use throughout the codebase
 module Constants
   NUMBER_OF_BOARD_FILES = 8
@@ -7,32 +9,42 @@ module Constants
   BOARD_FILES = ('a'..'h')
   BOARD_RANKS = (1..8)
 
-  SQUARE_COLOR_RGB_MAP = { yellow: '200, 194, 100',
-                           green: '119, 162, 109' }.freeze
+  SQUARE_COLOR_RGB_MAP = {
+    yellow: '200, 194, 100',
+    green: '119, 162, 109'
+  }.freeze
 
-  PIECE_COLOR_RGB_MAP = { white: '255, 255, 255',
-                          black: '0, 0, 0' }.freeze
+  PIECE_COLOR_RGB_MAP = {
+    white: '255, 255, 255',
+    black: '0, 0, 0'
+  }.freeze
 
-  WHITE_PIECE_FEN_MAP = { king: 'K',
-                          queen: 'Q',
-                          rook: 'R',
-                          bishop: 'B',
-                          knight: 'N',
-                          pawn: 'P' }.freeze
+  WHITE_PIECE_FEN_MAP = {
+    king: 'K',
+    queen: 'Q',
+    rook: 'R',
+    bishop: 'B',
+    knight: 'N',
+    pawn: 'P'
+  }.freeze
 
-  BLACK_PIECE_FEN_MAP = { king: 'k',
-                          queen: 'q',
-                          rook: 'r',
-                          bishop: 'b',
-                          knight: 'n',
-                          pawn: 'p' }.freeze
+  BLACK_PIECE_FEN_MAP = {
+    king: 'k',
+    queen: 'q',
+    rook: 'r',
+    bishop: 'b',
+    knight: 'n',
+    pawn: 'p'
+  }.freeze
 
-  PIECE_UNICODE_ICON_MAP = { king: "\u{265A}",
-                             queen: "\u{265B}",
-                             rook: "\u{265C}",
-                             bishop: "\u{265D}",
-                             knight: "\u{265E}",
-                             pawn: "\u{2659}" }.freeze
+  PIECE_UNICODE_ICON_MAP = {
+    king: "\u{265A}",
+    queen: "\u{265B}",
+    rook: "\u{265C}",
+    bishop: "\u{265D}",
+    knight: "\u{265E}",
+    pawn: "\u{2659}"
+  }.freeze
 
   PIECE_FEN_CLASS_MAP = {
     white: { K: King, Q: Queen, R: Rook, B: Bishop, N: Knight, P: Pawn },
