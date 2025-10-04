4b359ded810bf0b06c483d83beb07015e18e79f6
David H <151378521+Kezy-d4@users.noreply.github.com>
2025-10-04 15:13:20 -0400

    feat(Position): add #access_square

--- lib/position.rb
+++ lib/position.rb
@@ -10,6 +10,11 @@ class Position
     @instantiated_squares_and_pieces = instantiated_squares_and_pieces
   end
 
+  # untested
+  def access_square(algebraic_id)
+    @instantiated_squares_and_pieces[algebraic_id]
+  end
+
   class << self
     def from_fen(fen)
       new(fen_to_instantiated_algebraic(fen))
