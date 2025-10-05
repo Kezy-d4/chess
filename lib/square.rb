cca0cd4d576367334e8828cb376801eb09f117cf
David H <151378521+Kezy-d4@users.noreply.github.com>
2025-10-04 22:35:52 -0400

    feat(Square): add #empty? and #occupied?

--- lib/square.rb
+++ lib/square.rb
@@ -1,10 +1,22 @@
 # frozen_string_literal: true
 
+require_relative 'pieces'
+
 # A square on a chessboard
 class Square
   attr_reader :occupant
 
   def initialize(occupant = nil)
     @occupant = occupant
+  end
+
+  # untested
+  def empty?
+    @occupant.nil?
+  end
+
+  # untested
+  def occupied?
+    @occupant.is_a?(Piece)
   end
 end
