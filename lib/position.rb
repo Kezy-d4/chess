00375d00cd4d11ebb0cd434d402fb24391d9f6ed
David H <151378521+Kezy-d4@users.noreply.github.com>
2025-10-06 00:05:15 -0400

    feat: add ArgumentError guard clause to #rank_keys

--- lib/position.rb
+++ lib/position.rb
@@ -3,6 +3,7 @@
 require_relative 'fen_deserialization'
 require_relative 'square'
 require_relative 'pieces'
+require_relative 'constants'
 
 # The square and piece placement of a chess position
 class Position
@@ -20,6 +21,10 @@ class Position
     end
 
     def rank_keys(rank)
+      unless rank.is_a?(Integer) && rank.between?(Constants::BOARD_RANKS.min, Constants::BOARD_RANKS.max)
+        raise ArgumentError, 'Expected an Integer between one and eight'
+      end
+
       Constants::BOARD_FILES.each_with_object([]) do |file, arr|
         arr << :"#{file}#{rank}"
       end
