12321c50ca9b81c4fd56a162efec193c268b72f0
David H <151378521+Kezy-d4@users.noreply.github.com>
2025-10-07 00:08:40 -0400

    feat: extend Constants

--- lib/constants.rb
+++ lib/constants.rb
@@ -53,4 +53,7 @@ module Constants
     white: { King => 'K', Queen => 'Q', Rook => 'R', Bishop => 'B', Knight => 'N', Pawn => 'P' },
     black: { King => 'k', Queen => 'q', Rook => 'r', Bishop => 'b', Knight => 'n', Pawn => 'p' }
   }.freeze
+
+  BOARD_FILE_NUMBER_MAP = { 'a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5, 'f' => 6, 'g' => 7, 'h' => 8 }.freeze
+  NUMBER_BOARD_FILE_MAP = { 1 => 'a', 2 => 'b', 3 => 'c', 4 => 'd', 5 => 'e', 6 => 'f', 7 => 'g', 8 => 'h' }.freeze
 end
