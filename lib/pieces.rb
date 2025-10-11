d23e432b2aa7f8b59805c04b7a11c9ea4a0f511e
David H <151378521+Kezy-d4@users.noreply.github.com>
2025-10-11 13:14:26 -0400

    chore: fresh slate

--- lib/pieces.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-require_relative 'pieces/king'
-require_relative 'pieces/queen'
-require_relative 'pieces/rook'
-require_relative 'pieces/bishop'
-require_relative 'pieces/knight'
-require_relative 'pieces/pawn'
