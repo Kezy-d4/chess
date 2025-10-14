d6dddbf470d037c2631c934cbf9e69a327bedf6e
David H <151378521+Kezy-d4@users.noreply.github.com>
2025-10-14 15:11:15 -0400

    feat: add a few chess constants

---
+++ lib/chess_constants.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# A namespace to store chess constants for use throughout the codebase
+module ChessConstants
+  BOARD_RANKS = (1..8)
+  BOARD_FILES = ('a'..'h')
+  DEFAULT_FEN = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
+end
