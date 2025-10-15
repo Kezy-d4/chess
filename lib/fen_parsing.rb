ff174685b9d508c83117b638d45efc308c2456dd
David H <151378521+Kezy-d4@users.noreply.github.com>
2025-10-15 13:59:22 -0400

    refactor: convert fen parser into a mixin

---
+++ lib/fen_parsing.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require_relative 'chess_constants'
+
+# A mixin to parse a chess fen record
+module FenParsing
+  def data_fields(fen)
+    fen.split
+  end
+
+  def piece_placement_data(fen)
+    data_fields(fen)[0]
+  end
+end
---
+++ spec/fen_parsing_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require_relative '../lib/fen_parsing'
+
+describe FenParsing do
+  let(:dummy_class) { Class.new { extend FenParsing } }
+
+  describe '#data_fields' do
+    context 'when testing the default fen record' do
+      subject(:fen_default) { ChessConstants::DEFAULT_FEN }
+
+      it 'returns an array of substrings representing each data field' do
+        result = dummy_class.data_fields(fen_default)
+        expected = %w[rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1]
+        expect(result).to eq(expected)
+      end
+    end
+  end
+
+  describe '#piece_placement_data' do
+    context 'when testing the default fen record' do
+      subject(:fen_default) { ChessConstants::DEFAULT_FEN }
+
+      it 'returns a substring representing the piece placement data field' do
+        result = dummy_class.piece_placement_data(fen_default)
+        expected = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR'
+        expect(result).to eq(expected)
+      end
+    end
+  end
+end
