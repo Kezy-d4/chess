ac423062f5c3172608ef48d26aed42b939cbf511
David H <151378521+Kezy-d4@users.noreply.github.com>
2025-10-15 19:11:15 -0400

    feat: add piece fen class map

--- lib/chess_constants.rb
+++ lib/chess_constants.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative 'pieces'
+
 # A namespace to store chess constants for use throughout the codebase
 module ChessConstants
   BOARD_RANKS = (1..8)
@@ -13,5 +15,24 @@ module ChessConstants
     bishop: "\u{2657}",
     knight: "\u{2658}",
     pawn: "\u{2659}"
+  }.freeze
+
+  PIECE_FEN_CLASS_MAP = {
+    white: {
+      'K' => King,
+      'Q' => Queen,
+      'R' => Rook,
+      'B' => Bishop,
+      'N' => Knight,
+      'P' => Pawn
+    },
+    black: {
+      'k' => King,
+      'q' => Queen,
+      'r' => Rook,
+      'b' => Bishop,
+      'n' => Knight,
+      'p' => Pawn
+    }
   }.freeze
 end
