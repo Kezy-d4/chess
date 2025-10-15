891024508abe4440677a867fe6621b18397d872d
David H <151378521+Kezy-d4@users.noreply.github.com>
2025-10-15 14:56:50 -0400

    feat: add piece icon map

--- lib/chess_constants.rb
+++ lib/chess_constants.rb
@@ -5,4 +5,13 @@ module ChessConstants
   BOARD_RANKS = (1..8)
   BOARD_FILES = ('a'..'h')
   DEFAULT_FEN = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
+
+  PIECE_ICON_MAP = {
+    king: "\u{2654}",
+    queen: "\u{2655}",
+    rook: "\u{2656}",
+    bishop: "\u{2657}",
+    knight: "\u{2658}",
+    pawn: "\u{2659}"
+  }
 end
