7c0413495d51eb4335c338395822ef649161a6bf
David H <151378521+Kezy-d4@users.noreply.github.com>
2025-10-19 20:25:41 -0400

    feat: extend PIECE_UNICODE_MAP

--- lib/chess_constants.rb
+++ lib/chess_constants.rb
@@ -8,13 +8,23 @@ module ChessConstants
   BOARD_FILES = ('a'..'h')
   DEFAULT_FEN = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
 
-  PIECE_ICON_MAP = {
-    king: "\u{2654}",
-    queen: "\u{2655}",
-    rook: "\u{2656}",
-    bishop: "\u{2657}",
-    knight: "\u{2658}",
-    pawn: "\u{2659}"
+  PIECE_UNICODE_MAP = {
+    white: {
+      king: "\u{2654}",
+      queen: "\u{2655}",
+      rook: "\u{2656}",
+      bishop: "\u{2657}",
+      knight: "\u{2658}",
+      pawn: "\u{2659}"
+    },
+    black: {
+      king: "\u{265A}",
+      queen: "\u{265B}",
+      rook: "\u{265C}",
+      bishop: "\u{265D}",
+      knight: "\u{265E}",
+      pawn: "\u{265F}"
+    }
   }.freeze
 
   PIECE_FEN_CLASS_MAP = {
