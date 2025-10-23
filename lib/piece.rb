f73f1fe2749e865404504b3005d0e37c6382426e
David H <151378521+Kezy-d4@users.noreply.github.com>
2025-10-23 14:06:57 -0400

    feat: add #black? and #white?

--- lib/piece.rb
+++ lib/piece.rb
@@ -12,4 +12,12 @@ class Piece
   def initialize(color)
     @color = color
   end
+
+  def black?
+    @color == :black
+  end
+
+  def white?
+    @color == :white
+  end
 end
--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -1,3 +1,45 @@
 # frozen_string_literal: true
 
 require_relative '../lib/piece'
+
+describe Piece do
+  describe '#black?' do
+    context 'when the piece is black' do
+      subject(:piece_black) { described_class.new(:black) }
+
+      it 'returns true' do
+        result = piece_black.black?
+        expect(result).to be(true)
+      end
+    end
+
+    context 'when the piece is white' do
+      subject(:piece_white) { described_class.new(:white) }
+
+      it 'returns false' do
+        result = piece_white.black?
+        expect(result).to be(false)
+      end
+    end
+  end
+
+  describe '#white?' do
+    context 'when the piece is white' do
+      subject(:piece_white) { described_class.new(:white) }
+
+      it 'returns true' do
+        result = piece_white.white?
+        expect(result).to be(true)
+      end
+    end
+
+    context 'when the piece is black' do
+      subject(:piece_black) { described_class.new(:black) }
+
+      it 'returns false' do
+        result = piece_black.white?
+        expect(result).to be(false)
+      end
+    end
+  end
+end
