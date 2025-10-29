0d24f616268e8efac80fc20818b02b9edc8ce538
David H <151378521+Kezy-d4@users.noreply.github.com>
2025-10-29 14:51:36 -0400

    feat: add Constants namespace with DEFAULT_FEN

--- lib/constants.rb
+++ lib/constants.rb
@@ -1,1 +1,6 @@
 # frozen_string_literal: true
+
+# A namespace to store constants for use throughout the codebase
+module Constants
+  DEFAULT_FEN = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
+end
