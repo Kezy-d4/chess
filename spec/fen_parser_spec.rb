ee2e31346921a58de829f089d05f2058e3f02ab0
David H <151378521+Kezy-d4@users.noreply.github.com>
2025-10-29 15:29:05 -0400

    test: refactor #parse_fen spec

--- spec/fen_parser_spec.rb
+++ spec/fen_parser_spec.rb
@@ -5,43 +5,19 @@ require_relative '../lib/fen_parser'
 describe FenParser do
   describe '#parse_fen' do
     context 'when testing with the default fen record' do
-      subject(:fen_parser_default) { described_class.new(default_fen) }
+      subject(:fen_parser_default) { described_class.new(Constants::DEFAULT_FEN) }
 
-      let(:default_fen) { Constants::DEFAULT_FEN }
-
-      it 'returns a hash that stores a string of the piece placement data' do
-        result = fen_parser_default.parse_fen[:piece_placement]
-        expected = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR'
-        expect(result).to eq(expected)
+      let(:expected) do
+        { piece_placement: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR',
+          active_color: 'w',
+          castling_availability: 'KQkq',
+          en_passant_target: '-',
+          half_move_clock: '0',
+          full_move_number: '1' }
       end
 
-      it 'returns a hash that stores a string of the active color data' do
-        result = fen_parser_default.parse_fen[:active_color]
-        expected = 'w'
-        expect(result).to eq(expected)
-      end
-
-      it 'returns a hash that stores a string of the castling availability data' do
-        result = fen_parser_default.parse_fen[:castling_availability]
-        expected = 'KQkq'
-        expect(result).to eq(expected)
-      end
-
-      it 'returns a hash that stores a string of the en passant target data' do
-        result = fen_parser_default.parse_fen[:en_passant_target]
-        expected = '-'
-        expect(result).to eq(expected)
-      end
-
-      it 'returns a hash that stores a string of the half move clock data' do
-        result = fen_parser_default.parse_fen[:half_move_clock]
-        expected = '0'
-        expect(result).to eq(expected)
-      end
-
-      it 'returns a hash that stores a string of the full move number data' do
-        result = fen_parser_default.parse_fen[:full_move_number]
-        expected = '1'
+      it 'returns a hash storing each data field as a string' do
+        result = fen_parser_default.parse_fen
         expect(result).to eq(expected)
       end
     end
