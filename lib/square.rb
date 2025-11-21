7dcd93f9ceff16a29e3fd6646b5cd2eb15e485c9
David H <151378521+Kezy-d4@users.noreply.github.com>
2025-11-21 16:20:30 -0500

    feat: add public Square interface

--- lib/square.rb
+++ lib/square.rb
@@ -4,10 +4,30 @@ require_relative 'constants'
 
 # A square on a chess board
 class Square
+  attr_reader :occupant
+
   def initialize(algebraic_coords, occupant = nil)
     @algebraic_coords = algebraic_coords
     @occupant = occupant
   end
 
-  def determine_bg_color; end
+  def update_occupant(new_occupant)
+    @occupant = new_occupant
+  end
+
+  def remove_occupant
+    occupant_to_remove = @occupant
+    @occupant = nil
+    occupant_to_remove
+  end
+
+  def determine_bg_color
+    rank_coord = @algebraic_coords[-1].to_i
+    file_coord = ('a'..'z').to_a.index(@algebraic_coords[0]) + 1
+    if (rank_coord.even? && file_coord.even?) || (rank_coord.odd? && file_coord.odd?)
+      :black
+    else
+      :white
+    end
+  end
 end
