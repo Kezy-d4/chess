2b21442e93875710ce6d323d68d3990a115239b1
David H <151378521+Kezy-d4@users.noreply.github.com>
2025-11-24 16:22:19 -0500

    feat: add NumericCoords with basic interface

--- lib/constants.rb
+++ lib/constants.rb
@@ -3,4 +3,8 @@
 # A namespace to store constants for use throughout the codebase
 module Constants
   DEFAULT_FEN = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
+  BOARD_FILES = ('a'..'h')
+  BOARD_RANKS = (1..8)
+  BOARD_FILES_LENGTH = BOARD_FILES.to_a.length
+  BOARD_RANKS_LENGTH = BOARD_RANKS.size
 end
