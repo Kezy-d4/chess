8a7a559f7bedddab7c19d4f7dc3dc172c9cb7c2b
David H <151378521+Kezy-d4@users.noreply.github.com>
2025-12-01 16:36:03 -0500

    feat: add FenParser

--- lib/constants.rb
+++ lib/constants.rb
@@ -2,8 +2,6 @@
 
 # A namespace to store constants for use throughout the codebase
 module Constants
-  BOARD_FILES = ('a'..'h').to_a
-  BOARD_RANKS = (1..8).to_a.reverse
-  BOARD_FILES_LENGTH = BOARD_FILES.length
-  BOARD_RANKS_LENGTH = BOARD_RANKS.length
+  DEFAULT_FEN = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
+  LOWERCASE_ALPHABET = ('a'..'z')
 end
