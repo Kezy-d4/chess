b260ffd58e09439ca921faa7b15d9e3e2d6ca15b
David H <151378521+Kezy-d4@users.noreply.github.com>
2025-12-05 00:04:56 -0500

    feat: add Square interface

--- lib/square.rb
+++ lib/square.rb
@@ -4,11 +4,31 @@ require_relative 'constants'
 
 # A square on a chess board
 class Square
+  attr_reader :algebraic_coords, :occupant
+
   # @param algebraic_coords [String] the algebraic coordinates
   # @param occupant [Piece, nil] the occupying piece or nil
   def initialize(algebraic_coords, occupant = nil)
     @algebraic_coords = algebraic_coords
     @occupant = occupant
+  end
+
+  def occupied?
+    @occupant != nil
+  end
+
+  def unoccupied?
+    @occupant.nil?
+  end
+
+  def update_occupant(new_occupant)
+    @occupant = new_occupant
+  end
+
+  def remove_occupant
+    occupant_to_remove = @occupant
+    @occupant = nil
+    occupant_to_remove
   end
 
   def to_s
--- spec/square_spec.rb
+++ spec/square_spec.rb
@@ -3,6 +3,82 @@
 require_relative '../lib/square'
 
 describe Square do
+  describe '#occupied?' do
+    context 'when the square is occupied' do
+      subject(:square_occupied) { described_class.new('a8', piece) }
+
+      let(:piece) { double('piece') }
+
+      it 'returns true' do
+        result = square_occupied.occupied?
+        expect(result).to be(true)
+      end
+    end
+
+    context 'when the square is unoccupied' do
+      subject(:square_unoccupied) { described_class.new('a8') }
+
+      it 'returns false' do
+        result = square_unoccupied.occupied?
+        expect(result).to be(false)
+      end
+    end
+  end
+
+  describe '#unoccupied?' do
+    context 'when the square is unoccupied' do
+      subject(:square_unoccupied) { described_class.new('a8') }
+
+      it 'returns true' do
+        result = square_unoccupied.unoccupied?
+        expect(result).to be(true)
+      end
+    end
+
+    context 'when the square is occupied' do
+      subject(:square_occupied) { described_class.new('a8', piece) }
+
+      let(:piece) { double('piece') }
+
+      it 'returns false' do
+        result = square_occupied.unoccupied?
+        expect(result).to be(false)
+      end
+    end
+  end
+
+  describe '#update_occupant' do
+    subject(:square) { described_class.new('a8') }
+
+    let(:piece) { double('piece') }
+
+    it 'updates the occupant' do
+      expect { square.update_occupant(piece) }.to change \
+        { square.instance_variable_get(:@occupant) }.from(nil).to(piece)
+    end
+
+    it 'returns the new occupant' do
+      result = square.update_occupant(piece)
+      expect(result).to be(piece)
+    end
+  end
+
+  describe '#remove_occupant' do
+    subject(:square) { described_class.new('a8', piece) }
+
+    let(:piece) { double('piece') }
+
+    it 'removes the occupant' do
+      expect { square.remove_occupant }.to change \
+        { square.instance_variable_get(:@occupant) }.from(piece).to(nil)
+    end
+
+    it 'returns the removed occupant' do
+      result = square.remove_occupant
+      expect(result).to be(piece)
+    end
+  end
+
   describe '#to_s' do
     context 'when the square is occupied' do
       subject(:square_occupied) { described_class.new('a8', piece) }
