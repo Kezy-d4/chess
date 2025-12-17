14407e938dae872e5397949aec5c64390d55677a
David H <151378521+Kezy-d4@users.noreply.github.com>
2025-12-17 00:39:11 -0500

    feat: add Player superclass

---
+++ lib/player.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require_relative 'constants'
+
+# A superclass to each of the chess players
+class Player
+  # @param name [String]
+  def initialize(name)
+    @name = name
+  end
+end
