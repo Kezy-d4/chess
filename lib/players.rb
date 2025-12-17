918abbda22b92f83e49b10a1dd6b2bf942f9681f
David H <151378521+Kezy-d4@users.noreply.github.com>
2025-12-17 00:41:33 -0500

    feat: add players

---
+++ lib/players.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+require_relative 'players/white_player'
+require_relative 'players/black_player'
---
+++ lib/players/white_player.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+require_relative '../player'
+
+# The chess player who plays white
+class WhitePlayer < Player; end
