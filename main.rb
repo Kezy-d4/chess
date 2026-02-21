746defa06331a246b2f9e90d3a8271829d534862
David H <151378521+Kezy-d4@users.noreply.github.com>
2026-02-21 18:57:05 -0500

    feat: add manual main script

--- main.rb
+++ main.rb
@@ -2,23 +2,108 @@
 
 require_relative 'lib/chess'
 
-# Main menu and player naming
-Chess::CLI.main_menu_prompt_loop
-player_white_name = Chess::CLI.player_name_prompt_loop(:white)
-player_black_name = Chess::CLI.player_name_prompt_loop(:black)
-Chess::CLI.welcome_players(player_white_name, player_black_name)
-
-# Setting up state
+# setup
 fen = Chess::ChessConstants::FEN_DEFAULT
 f_p = Chess::FENParser.new(fen)
 board = Chess::Board.from_fen_parser(f_p)
 aux_pos_data = Chess::AuxPosData.from_fen_parser(f_p)
-player_white = Chess::Player.new(player_white_name, :white)
-player_black = Chess::Player.new(player_black_name, :black)
-metadata = Chess::Position.new_default_metadata
-position = Chess::Position.new(board, aux_pos_data, player_white, player_black, metadata)
+player_white = Chess::Player.new('w', :white)
+player_black = Chess::Player.new('b', :black)
+log = Chess::Log.new({})
+position = Chess::Position.new(board, aux_pos_data, player_white, player_black, log)
 display = Chess::Display.new(board)
-cli = Chess::CLI.new(position, display)
 
-# Playing the game
-cli.play
+# initial
+puts "Initial:\n\n"
+display.render_board(position.dump_log[:metadata])
+
+# 1.
+puts "1.\n\n"
+
+position.select_source(Chess::Coord.from_s('b1'))
+display.render_board(position.dump_log[:metadata])
+puts
+
+position.move(position.dump_log[:metadata][:current_source], Chess::Coord.from_s('c3'))
+position.swap_active_player
+position.deselect_source
+display.render_board(position.dump_log[:metadata])
+
+# 1...
+puts "1...\n\n"
+
+position.select_source(Chess::Coord.from_s('e7'))
+display.render_board(position.dump_log[:metadata])
+puts
+
+position.move(position.dump_log[:metadata][:current_source], Chess::Coord.from_s('e5'))
+position.swap_active_player
+position.deselect_source
+display.render_board(position.dump_log[:metadata])
+
+# 2.
+puts "2.\n\n"
+
+position.select_source(Chess::Coord.from_s('d2'))
+display.render_board(position.dump_log[:metadata])
+puts
+
+position.move(position.dump_log[:metadata][:current_source], Chess::Coord.from_s('d4'))
+position.swap_active_player
+position.deselect_source
+display.render_board(position.dump_log[:metadata])
+
+# 2...
+puts "2...\n\n"
+
+position.select_source(Chess::Coord.from_s('e5'))
+display.render_board(position.dump_log[:metadata])
+puts
+
+position.move(position.dump_log[:metadata][:current_source], Chess::Coord.from_s('d4'))
+position.swap_active_player
+position.deselect_source
+display.render_board(position.dump_log[:metadata])
+
+# 3.
+puts "3.\n\n"
+
+position.select_source(Chess::Coord.from_s('d1'))
+display.render_board(position.dump_log[:metadata])
+puts
+
+position.move(position.dump_log[:metadata][:current_source], Chess::Coord.from_s('d4'))
+position.swap_active_player
+position.deselect_source
+display.render_board(position.dump_log[:metadata])
+
+# 3...
+puts "3...\n\n"
+
+position.select_source(Chess::Coord.from_s('d8'))
+display.render_board(position.dump_log[:metadata])
+puts
+
+position.move(position.dump_log[:metadata][:current_source], Chess::Coord.from_s('f6'))
+position.swap_active_player
+position.deselect_source
+display.render_board(position.dump_log[:metadata])
+
+# 4.
+puts "4.\n\n"
+
+position.select_source(Chess::Coord.from_s('g1'))
+display.render_board(position.dump_log[:metadata])
+puts
+
+position.move(position.dump_log[:metadata][:current_source], Chess::Coord.from_s('f3'))
+position.swap_active_player
+position.deselect_source
+display.render_board(position.dump_log[:metadata])
+
+# 4...
+puts "4...\n\n"
+
+position.select_source(Chess::Coord.from_s('f6'))
+display.render_board(position.dump_log[:metadata])
+puts
