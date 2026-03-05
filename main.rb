77c72d425e3ddb3608999d14e3eec7ba052a5bc7
David H <151378521+Kezy-d4@users.noreply.github.com>
2026-03-05 16:43:04 -0500

    feat: add main script

--- main.rb
+++ main.rb
@@ -2,10 +2,8 @@
 
 require_relative 'lib/chess'
 
-original = Chess::Position.new_default('w', 'b')
-clone = original.clone
-clone.move(Chess::Coord.from_s('e2'), Chess::Coord.from_s('e4'))
-
-# The original is being modified and shouldn't be. This has been the case for
-# each of my attempted implementations.
-puts original
+fen_parser = Chess::FENParser.new(Chess::ChessConstants::FEN_DEFAULT)
+position = Chess::Position.from_fen_parser(fen_parser, 'w', 'b')
+display = Chess::Display.new
+game = Chess::Game.new(position, display)
+game.play
