198fa273b5a80cc52f25ae62ad6a0384b4d2c872
David H <151378521+Kezy-d4@users.noreply.github.com>
2026-03-05 16:42:31 -0500

    feat: extend Game

--- lib/chess/game.rb
+++ lib/chess/game.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 module Chess
-  # a game of chess
+  # A game of chess
   class Game
+    SEPARATOR = '------------------------------------------------------------' \
+                '-------------------------------------'
+
     using StringExtensions
 
     # @param position [Position]
@@ -13,83 +16,107 @@ module Chess
     end
 
     def play
+      take_turns_until_over
+      display_game_over_ux
+    end
+
+    def take_turns_until_over # rubocop:disable Metrics/MethodLength,Metrics/AbcSize
       loop do
-        turn
+        system('clear')
+        render_board_with_separator
+        source_inp = prompt_for_source
+        @position.select_source(Coord.from_s(source_inp))
+        system('clear')
+        render_board_with_separator
+        post_source_inp = prompt_for_post_source(Coord.from_s(source_inp))
+        if legal_escape_input?(post_source_inp)
+          @position.deselect_source
+          next
+        elsif legal_destination_input?(Coord.from_s(source_inp), post_source_inp)
+          @position.move(Coord.from_s(source_inp), Coord.from_s(post_source_inp))
+          @position.deselect_source
+          @position.swap_active_player
+          break if over?
+        end
       end
     end
 
-    def turn # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
-      loop do
-        render_board_with_separator
-        @position.select_source(Coord.from_s(prompt_for_source))
-        system('clear')
-        render_board_with_separator
-        destination = prompt_for_destination
-        if valid_escape_input?(destination)
-          @position.deselect_source
-          system('clear')
-          next
-        end
-        @position.move(@position.dump_log[:metadata][:current_source], Coord.from_s(destination))
-        @position.deselect_source
-        @position.swap_active_player
-        system('clear')
-      end
+    def over?
+      @position.checkmate? || @position.stalemate?
     end
 
     def prompt_for_source
-      sources = @position.to_active_player_sources.map(&:to_s)
       loop do
-        puts "#{@position.to_active_player}, it's your turn."
-        puts "Select one of your squares:\n#{sources}"
+        display_source_prompt_ux
         input = gets.chomp
-        return input if valid_source_input?(input)
+        return input if legal_source_input?(input)
 
-        print_invalid_submission_msg(input)
+        display_invalid_input_ux(input)
+        puts SEPARATOR
       end
     end
 
-    def prompt_for_destination # rubocop:disable Metrics/MethodLength
-      return unless @position.source?
-
-      source = @position.dump_log[:metadata][:current_source]
-      destinations = @position.to_destinations_from(source).map(&:to_s)
+    def prompt_for_post_source(source)
       loop do
-        puts "#{@position.to_active_player}, it's your turn."
-        puts "Select a destination:\n#{destinations}"
-        puts 'Alternatively, submit "esc" to return and select another square.'
+        display_post_source_prompt_ux(source)
         input = gets.chomp
-        return input if valid_destination_input?(input) || valid_escape_input?(input)
+        return input if legal_escape_input?(input) || legal_destination_input?(source, input)
 
-        print_invalid_submission_msg(input)
+        display_invalid_input_ux(input)
+        puts SEPARATOR
       end
     end
 
-    def valid_source_input?(input)
-      input.valid_coord? && @position.valid_source?(Coord.from_s(input))
+    def legal_source_input?(input)
+      input.valid_coord? && @position.legal_source?(Coord.from_s(input))
     end
 
-    def valid_destination_input?(input)
-      return false unless @position.source?
-
-      source = @position.dump_log[:metadata][:current_source]
-      input.valid_coord? && @position.valid_move?(source, Coord.from_s(input))
+    def legal_destination_input?(source, input)
+      input.valid_coord? && @position.legal_move?(source, Coord.from_s(input))
     end
 
-    def valid_escape_input?(input)
+    def legal_escape_input?(input)
       input.downcase == 'esc'
     end
 
-    private
+    def display_source_prompt_ux
+      display_prompt_ux
+      puts 'Select one of your squares:'
+      puts @position.to_player_sources(@position.to_active_player).map(&:to_s).inspect
+    end
+
+    def display_post_source_prompt_ux(source)
+      display_prompt_ux
+      puts 'Select a destination:'
+      puts @position.to_legal_destinations_from(source).map(&:to_s).inspect
+      puts 'Alternatively, submit "esc" to return and select another square:'
+    end
+
+    def display_prompt_ux
+      if @position.check?
+        puts "#{@position.to_active_player}, you're in check! Your next move must remove check."
+      else
+        puts "#{@position.to_active_player}, it's your turn."
+      end
+    end
+
+    def display_game_over_ux
+      system('clear')
+      render_board_with_separator
+      if @position.checkmate?
+        puts "#{@position.to_inactive_player} wins by checkmate!"
+      elsif @position.stalemate?
+        puts 'Draw by stalemate!'
+      end
+    end
+
+    def display_invalid_input_ux(input)
+      puts "Invalid input: #{input}"
+    end
 
     def render_board_with_separator
       @display.render_board(@position.to_board_ranks, @position.dump_log[:metadata])
-      puts UX::SEPARATOR
-    end
-
-    def print_invalid_submission_msg(input)
-      puts "Invalid submission: #{input}"
-      puts UX::SEPARATOR
+      puts SEPARATOR
     end
   end
 end
