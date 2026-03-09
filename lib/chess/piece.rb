906406ac494881d737c6735f6b4294d84f6a1d26
David H <151378521+Kezy-d4@users.noreply.github.com>
2026-03-08 23:00:24 -0400

    refactor: remove Piece total_moves instance var
    This instance variable was added initially in an attempt to anticipate
    future features such as the special moves. It overcomplicated code and
    caused testing difficulties, so it's been removed.

--- lib/chess/piece.rb
+++ lib/chess/piece.rb
@@ -3,15 +3,13 @@
 module Chess
   # A superclass to each of the chess pieces
   class Piece
-    attr_reader :color, :total_moves
+    attr_reader :color
 
     using ObjectExtensions
 
     # @param color [Symbol]
-    # @param total_moves [Integer]
-    def initialize(color, total_moves = 0)
+    def initialize(color)
       @color = color
-      @total_moves = total_moves
     end
 
     def white?
@@ -22,34 +20,8 @@ module Chess
       @color == :black
     end
 
-    def moved?
-      @total_moves.positive?
-    end
-
-    def moved_once?
-      @total_moves == 1
-    end
-
-    def moved_more_than_once?
-      @total_moves > 1
-    end
-
-    def unmoved?
-      @total_moves.zero?
-    end
-
-    def increment_total_moves
-      @total_moves += 1
-    end
-
     def to_s
-      if unmoved?
-        "The #{to_class_s} is #{@color} and has not moved."
-      elsif moved_once?
-        "The #{to_class_s} is #{@color} and has moved #{@total_moves} time."
-      elsif moved_more_than_once?
-        "The #{to_class_s} is #{@color} and has moved #{@total_moves} times."
-      end
+      "The #{to_class_s} is #{@color}."
     end
   end
 end
--- spec/chess/piece_spec.rb
+++ spec/chess/piece_spec.rb
@@ -1,9 +1,5 @@
 # frozen_string_literal: true
 
-# NOTE: For this spec and its sub-specs, if the @color parameter is not relevant
-# to the specific test, then we always set it to :white as a placeholder.
-# Additionally, it's important to know that the @total_moves parameter defaults
-# to 0.
 describe Chess::Piece do
   describe '#white?' do
     context 'when white' do
@@ -45,139 +41,12 @@ describe Chess::Piece do
     end
   end
 
-  describe '#moved?' do
-    context 'when moved at least once' do
-      subject(:piece_moved) { described_class.new(:white, 1) }
-
-      it 'returns true' do
-        result = piece_moved.moved?
-        expect(result).to be(true)
-      end
-    end
-
-    context 'when unmoved' do
-      subject(:piece_unmoved) { described_class.new(:white) }
-
-      it 'returns false' do
-        result = piece_unmoved.moved?
-        expect(result).to be(false)
-      end
-    end
-  end
-
-  describe '#moved_once?' do
-    context 'when moved exactly once' do
-      subject(:piece_moved_once) { described_class.new(:white, 1) }
-
-      it 'returns true' do
-        result = piece_moved_once.moved_once?
-        expect(result).to be(true)
-      end
-    end
-
-    context 'when moved more than once' do
-      subject(:piece_moved_multiple_times) { described_class.new(:white, 2) }
-
-      it 'returns false' do
-        result = piece_moved_multiple_times.moved_once?
-        expect(result).to be(false)
-      end
-    end
-
-    context 'when unmoved' do
-      subject(:piece_unmoved) { described_class.new(:white) }
-
-      it 'returns false' do
-        result = piece_unmoved.moved_once?
-        expect(result).to be(false)
-      end
-    end
-  end
-
-  describe '#moved_more_than_once?' do
-    context 'when moved more than once' do
-      subject(:piece_moved_multiple_times) { described_class.new(:white, 2) }
-
-      it 'returns true' do
-        result = piece_moved_multiple_times.moved_more_than_once?
-        expect(result).to be(true)
-      end
-    end
-
-    context 'when moved exactly once' do
-      subject(:piece_moved_once) { described_class.new(:white, 1) }
-
-      it 'returns false' do
-        result = piece_moved_once.moved_more_than_once?
-        expect(result).to be(false)
-      end
-    end
-
-    context 'when unmoved' do
-      subject(:piece_unmoved) { described_class.new(:white) }
-
-      it 'returns false' do
-        result = piece_unmoved.moved_more_than_once?
-        expect(result).to be(false)
-      end
-    end
-  end
-
-  describe '#unmoved?' do
-    context 'when unmoved' do
-      subject(:piece_unmoved) { described_class.new(:white) }
-
-      it 'returns true' do
-        result = piece_unmoved.unmoved?
-        expect(result).to be(true)
-      end
-    end
-
-    context 'when moved at least once' do
-      subject(:piece_moved) { described_class.new(:white, 1) }
-
-      it 'returns false' do
-        result = piece_moved.unmoved?
-        expect(result).to be(false)
-      end
-    end
-  end
-
-  describe '#increment_total_moves' do
+  describe '#to_s' do
     subject(:piece) { described_class.new(:white) }
 
-    it 'increments @total_moves by one' do
-      expect { piece.increment_total_moves }.to change \
-        { piece.instance_variable_get(:@total_moves) }.from(0).to(1)
-    end
-  end
-
-  describe '#to_s' do
-    context 'when unmoved' do
-      subject(:piece_unmoved) { described_class.new(:white) }
-
-      it 'returns a string describing the state' do
-        result = piece_unmoved.to_s
-        expect(result).to eq('The Piece is white and has not moved.')
-      end
-    end
-
-    context 'when moved exactly once' do
-      subject(:piece_moved_once) { described_class.new(:white, 1) }
-
-      it 'returns a string describing the state' do
-        result = piece_moved_once.to_s
-        expect(result).to eq('The Piece is white and has moved 1 time.')
-      end
-    end
-
-    context 'when moved more than once' do
-      subject(:piece_moved_multiple_times) { described_class.new(:white, 8) }
-
-      it 'returns a string describing the state' do
-        result = piece_moved_multiple_times.to_s
-        expect(result).to eq('The Piece is white and has moved 8 times.')
-      end
+    it 'returns a string describing the state' do
+      result = piece.to_s
+      expect(result).to eq('The Piece is white.')
     end
   end
 end
