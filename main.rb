a4d7bfc90dc163b305acb6a79e10a9c4094a21a5
David H <151378521+Kezy-d4@users.noreply.github.com>
2026-03-20 15:40:41 -0400

    chore: clear main script

--- main.rb
+++ main.rb
@@ -1,9 +1,4 @@
 # frozen_string_literal: true
 
 require_relative 'lib/chess'
-
-fen_parser = Chess::FENParser.new(Chess::ChessConstants::FEN_DEFAULT)
-position = Chess::Position.from_fen_parser(fen_parser, 'w', 'b')
-display = Chess::Display.new
-game = Chess::Game.new(position, display)
-game.play
+require 'pry-byebug'
